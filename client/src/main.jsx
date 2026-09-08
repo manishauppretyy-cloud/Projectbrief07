e98e99a913f674185118d96181398b8ec823e2e2
Manisha Upprety <manishauppretyy@gmail.com>
2026-09-08 17:55:31 +0530

    complete sprint 8

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/components.css";
+import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
