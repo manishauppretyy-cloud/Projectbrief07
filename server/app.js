21dc3cee8baa8de4f4219e2807f8356c1b73cd39
manishauppretyy-cloud <manishauppretyy@gmail.com>
2026-09-12 22:03:20 +0530

    sprint 11 completed

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const testRoutes = require("./routes/testRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/test", testRoutes);
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("Job Portal API is running successfully!");
-});
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
